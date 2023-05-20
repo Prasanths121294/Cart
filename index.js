efc348d12641db158555498ab980314dfdb671c1
Prasanths121294 <132745731+Prasanths121294@users.noreply.github.com>
2023-05-20 11:56:21 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
 
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
-
-
-const appSettings = {
-   databaseURL: "https://cart-7c2f5-default-rtdb.asia-southeast1.firebasedatabase.app/"
-}
+   import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 
-const app = initializeApp(appSettings)
-const database = getDatabase(app)
-const cartInDB = ref(database, "myCart")
+   const appSettings = {
+      databaseURL: "https://cart-7c2f5-default-rtdb.asia-southeast1.firebasedatabase.app/"
+   }
 
- 
- 
- const inputFieldEl=document.getElementById("input-field")
- const addButtonEl=document.getElementById("add-button")
-   const orderListEl=document.getElementById("order-list")
-   console.log("test")
+
+   const app = initializeApp(appSettings)
+   const database = getDatabase(app)
+   const cartInDB = ref(database, "myCart")
+
+   
+   
+   const inputFieldEl=document.getElementById("input-field")
+   const addButtonEl=document.getElementById("add-button")
+      const orderListEl=document.getElementById("order-list")
+   
    onValue(cartInDB,function(snapshot){
-      let listItem=Object.values(snapshot.val())
-      clearShoppingListEl()
-      for (let i=0; i<listItem.length; i++)
-      {
-         console.log(i)
-         let currentBook=listItem[i]
-         loadListValue(currentBook)
-      }
+            let listItem=Object.values(snapshot.val())
+            clearShoppingListEl()
+            for (let i=0; i<listItem.length; i++)
+            {
+            
+               let currentBook=listItem[i]
+               loadListValue(currentBook)
+            }
+         })
+      
+   addButtonEl.addEventListener("click",function(){ 
+      let inputValue=inputFieldEl.value
+      push(cartInDB,inputValue)
+      
+      clearInputFieldEl()
+      
    })
    
- addButtonEl.addEventListener("click",function(){ 
-    let inputValue=inputFieldEl.value
-    push(cartInDB,inputValue)
-    
-    clearInputFieldEl()
-    loadListValue(inputValue)
- })
- 
- function clearShoppingListEl() {
-   orderListEl.innerHTML = ""
-}
+   function clearShoppingListEl() {
+      orderListEl.innerHTML = ""
+   }
 
-function clearInputFieldEl() {
-   inputFieldEl.value = ""
-}
+   function clearInputFieldEl() {
+      inputFieldEl.value = ""
+   }
 
- function loadListValue(inputval){
-   orderListEl.innerHTML += `<li>${inputval}</li>`
- }
+   function loadListValue(inputval){
+      orderListEl.innerHTML += `<li>${inputval}</li>`
+   }
