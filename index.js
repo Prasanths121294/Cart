1b9a5bb64ed37290e21ffef2491995819619e569
Prasanths121294 <132745731+Prasanths121294@users.noreply.github.com>
2023-05-23 12:53:59 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,71 +1,66 @@
-
-
    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+   import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
-         import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+      const appSettings = {
+                        databaseURL: "https://cart-7c2f5-default-rtdb.asia-southeast1.firebasedatabase.app/"
+                           }
 
+      const app = initializeApp(appSettings)
+      const database = getDatabase(app)
+      const cartInDB = ref(database, "myCart")
 
-   const appSettings = {
-      databaseURL: "https://cart-7c2f5-default-rtdb.asia-southeast1.firebasedatabase.app/"
-   }
-
-
-   const app = initializeApp(appSettings)
-   const database = getDatabase(app)
-   const cartInDB = ref(database, "myCart")
-
-   const inputFieldEl=document.getElementById("input-field")
-   const addButtonEl=document.getElementById("add-button")
+      const inputFieldEl=document.getElementById("input-field")
+      const addButtonEl=document.getElementById("add-button")
       const orderListEl=document.getElementById("order-list")
-   
-             onValue(cartInDB,function(snapshot){
+//---------------------------------------
+         onValue(cartInDB,function(snapshot){
             
-            if(snapshot.exists())
-            {
-               let listItem=Object.entries(snapshot.val())
-            clearShoppingListEl()
-            
-            for (let i=0; i<listItem.length; i++)
-            {
-               let currentItem=listItem[i]
+               if(snapshot.exists())
+               {
+                  let listItem=Object.entries(snapshot.val())
+                  console.log(listItem +"1")
+                  clearShoppingListEl()
+                     for (let i=0; i<listItem.length; i++)
+                     {
+                        let currentItem=listItem[i]
 
-               let currenItemName=currentItem[1]
-               let currenItemId=currentItem[0]
-               loadListValue(currentItem)
-            }
-         }
-         else{
-            orderListEl.innerHTML = "No items here..."
-         }
+                        let currenItemName=currentItem[1]
+                        let currenItemId=currentItem[0]
+                        loadListValue(currentItem)
+                     }
+               }
+               else{
+                     orderListEl.innerHTML = "No items here..."
+               }
          })
-      
-   addButtonEl.addEventListener("click",function(){ 
-      let inputValue=inputFieldEl.value
-      push(cartInDB,inputValue)
-      
-      clearInputFieldEl()
-      
-   })
-   
-   function clearShoppingListEl() {
-      orderListEl.innerHTML = ""
-   }
+//---------------------------------------------------------------------
+         addButtonEl.addEventListener("click",function(){ 
+               let inputValue=inputFieldEl.value
+               push(cartInDB,inputValue)
+               clearInputFieldEl()
+         
+         })
+//--------------------------------------------------------------------- 
+         function clearShoppingListEl() {
+               orderListEl.innerHTML = ""
+         }
 
-   function clearInputFieldEl() {
-      inputFieldEl.value = ""
-   }
-
-   function loadListValue(inputval){
-     let itemName=inputval[1]
-     let itemId=inputval[0]
-     let newEl=document.createElement("li")
-     
-     orderListEl.append(newEl)
-
-     newEl.addEventListener("click",function(){
-      let deltItem=ref(database,`myCart/${itemId}`)
-      remove(deltItem)
-     })
-     newEl.textContent=itemName
-   }
+         function clearInputFieldEl() {
+               inputFieldEl.value = ""
+         }
+//--------------------------------------------------------------------
+         function loadListValue(inputval){
+            console.log(inputval+"2")
+               let itemName=inputval[1]
+               let itemId=inputval[0]
+               let newEl=document.createElement("li")
+               newEl.textContent=itemName
+               orderListEl.append(newEl)
+               newEl.addEventListener("click",function(){
+                  let deltItem=ref(database,`myCart/${itemId}`)
+                  console.log(newEl+"3")
+                  remove(deltItem)
+               })
+              
+         }
 
